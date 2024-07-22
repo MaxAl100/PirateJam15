b2e32b970fcbe1c28be0cb7fef53456e2bc794be
MaxAl100 <43099597+MaxAl100@users.noreply.github.com>
2024-07-22 13:01:07 -0400

    4 basic elements

---
+++ Scripts/Attacks/Basic Elements/AirShove.gd
@@ -0,0 +1,29 @@
+extends Area2D
+
+var damage = 2
+var amount = 1
+var maxTimeBetweenAttacks = 1
+var currentTimeBetweenAttacks = 1
+var attackLength = 0.06
+var target = "looking side"
+var direction = Vector2.ZERO
+var knockback = 100
+
+func _ready():
+	connect("body_entered", Callable(self, "_on_Bullet_body_entered"))
+
+func _physics_process(delta):
+	attackLength -= delta
+	if attackLength <= 0:
+		queue_free()
+
+func _on_Bullet_body_entered(body):
+	if body.name == "Player":
+		return
+	if body.is_in_group("enemies"):
+		body._recieve_damage(self)
+
+func set_direction_and_rotate(direction):
+	self.direction = direction
+	rotation = direction.angle()
+
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,20 +1,25 @@
 extends CharacterBody2D
 
-@export var Speed = 80.0
+@export var Speed = 40.0
 @export var Bullets: Array[PackedScene] = []
 var timesForBullets: Array[float] = []
 @export var Health = 100.0
-@export var MaxInvincibilityTime = 0.4
+@export var MaxInvincibilityTime = 0.8
 var CurrentInvincibilityTime = 0
+var damage = 0
+
+@export var KnockbackStrength = 20.0
+
+var _last_direction = Vector2.RIGHT
 
 func _ready():
 	timesForBullets.resize(Bullets.size())
 	for i in range(Bullets.size()):
 		var bullet_instance = Bullets[i].instantiate()
-		timesForBullets[i] = bullet_instance.maxTimeBetweenAttacks
+		timesForBullets[i] = bullet_instance.currentTimeBetweenAttacks
 
 func _physics_process(delta):
-	velocity = Vector2.ZERO  # Use Vector2.ZERO to reset velocity
+	velocity = Vector2.ZERO
 
 	if Input.is_action_pressed("move_down"):
 		velocity.y += Speed
@@ -22,8 +27,10 @@ func _physics_process(delta):
 		velocity.y -= Speed
 	if Input.is_action_pressed("move_left"):
 		velocity.x -= Speed
+		_last_direction = Vector2.LEFT
 	if Input.is_action_pressed("move_right"):
 		velocity.x += Speed
+		_last_direction = Vector2.RIGHT
 
 	move_and_slide()
 
@@ -34,7 +41,7 @@ func _physics_process(delta):
 		var collision_name = collision.get_collider().name
 		if "Enemy" in collision_name:
 			_recieve_damage(collision)
-	
+
 	for i in range(timesForBullets.size()):
 		timesForBullets[i] -= delta
 		if timesForBullets[i] <= 0:
@@ -43,22 +50,47 @@ func _physics_process(delta):
 			timesForBullets[i] = bullet_instance.maxTimeBetweenAttacks
 
 func _recieve_damage(collision):
-	var damage = collision.get_collider().Damage
-	if CurrentInvincibilityTime <= 0:
+	if CurrentInvincibilityTime > 0:
+		return
+	CurrentInvincibilityTime = MaxInvincibilityTime
+	var collider = collision.get_collider()
+	if collider.has_method("get_damage"):
+		var coll_damage = collider.get_damage()
+		if CurrentInvincibilityTime > 0:
+			return
 		CurrentInvincibilityTime = MaxInvincibilityTime
-		Health -= damage
+		Health -= coll_damage
+
+		if Health <= 0:
+			self.queue_free()
+			get_tree().reload_scene()
+		else:
+			_apply_knockback(collision)
+
+func _apply_knockback(collision):
+	var collider = collision.get_collider()
+	var knockback_direction = (position - collider.position).normalized()
+	velocity += knockback_direction * KnockbackStrength
 
 func _shoot_bullet(bullet_scene):
 	var newBullet = bullet_scene.instantiate()
 	var all_enemies = get_tree().get_nodes_in_group("enemies")
+
+	if newBullet.target == "nearest enemy" && all_enemies.size() > 0:
+		var closest_enemy = all_enemies[0]
+		for enemy in all_enemies:
+			if position.distance_to(enemy.position) < position.distance_to(closest_enemy.position):
+				closest_enemy = enemy
+		var direction = (closest_enemy.position - position).normalized()
+		newBullet.position = $"Bullet Origin".position
+		newBullet.set_direction_and_rotate(direction)
+		add_child(newBullet)
 	
-	var closest_enemy = all_enemies[0]
-	for enemy in all_enemies:
-		if position.distance_to(enemy.position) < position.distance_to(closest_enemy.position):
-			closest_enemy = enemy
-
-	if newBullet.target == "nearest enemy" && all_enemies.size > 0:
-		newBullet.direction = (closest_enemy.position - position).normalized()
-		newBullet.position = $"Bullet Origin".position 
-		add_child(newBullet)  
-
+	elif newBullet.target == "self":
+		newBullet.position = $"Bullet Origin".position
+		add_child(newBullet)
+		
+	elif newBullet.target == "looking side":
+		newBullet.set_direction_and_rotate(_last_direction)
+		newBullet.position = $"Bullet Origin".position
+		add_child(newBullet)
