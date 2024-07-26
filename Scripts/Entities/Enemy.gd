68e2762ce3543672fa549d9428596b72f750a8a9
MaxAl100 <43099597+MaxAl100@users.noreply.github.com>
2024-07-25 20:05:28 -0400

    Fixes

--- Scripts/Entities/Enemy.gd
+++ Scripts/Entities/Enemy.gd
@@ -4,12 +4,14 @@ extends CharacterBody2D
 @export var Speed = 15.0
 var SlowDownEffect = 1
 var DamageMultiplier = 1
-@export var Health = 3.0
+@export var Health = 80.0
 
 @export var InvincibilityTime = 0.5
+@export var TotalInvincibilityTime = 0.1
 @export var KnockbackResistance = 0.4
 var Invincibilities: Array[Area2D] = []
 var currInvincibility: Array[float] = []
+var TotalInvincibility = 0
 
 var Player
 var Direction
@@ -20,23 +22,28 @@ func _ready():
 
 
 func _physics_process(delta):
-	for pos in currInvincibility.size():
+	if TotalInvincibility > 0:
+		TotalInvincibility -= delta
+	for pos in range(currInvincibility.size() - 1, -1, -1):
 		currInvincibility[pos] -= delta
 		if currInvincibility[pos] <= 0:
 			currInvincibility.remove_at(pos)
 			Invincibilities.remove_at(pos)
+
 	if SlowDownEffect > 1:
 		SlowDownEffect = 1
 	elif SlowDownEffect < 1:
 		SlowDownEffect += 0.5 * delta
+
 	if Health > 0:
 		Direction = (Player.position - self.position).normalized()
 		velocity = Direction * Speed * SlowDownEffect
 		move_and_slide()
 
 func _recieve_damage(entity):
-	if entity in Invincibilities:
+	if entity in Invincibilities or TotalInvincibility > 0:
 		return
+	TotalInvincibility = TotalInvincibilityTime
 	var rec_damage = entity.damage
 	Invincibilities.append(entity)
 	currInvincibility.append(InvincibilityTime)
