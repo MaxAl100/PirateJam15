bb8af5d59c25a2bb7059b69d309567e984afb558
MaxAl100 <43099597+MaxAl100@users.noreply.github.com>
2024-07-28 20:56:42 -0400

    Fixes

--- Scripts/Entities/PickUpElement.gd
+++ Scripts/Entities/PickUpElement.gd
@@ -2,8 +2,14 @@ extends CharacterBody2D
 
 @export var PossibleElements: Array[PackedScene]
 var SelectedElement
+var SelectedSprite
+var Sprite
 var Selector = RandomNumberGenerator.new()
 
 func _ready():
 	SelectedElement = PossibleElements[Selector.randi_range(0,PossibleElements.size()-1)]
+	Sprite = $Sprite2D
+	SelectedSprite = Selector.randi_range(0,23)
+	Sprite.region_rect = Rect2(Vector2((SelectedSprite%12 * 8), floor(SelectedSprite/12) * 8), Vector2(8, 8))
+
 	print(SelectedElement.resource_name)
